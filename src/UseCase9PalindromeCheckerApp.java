247d9499169e87e3cea8f4104026b1ac6ebe329a
arnavanand26 <aa4967@srmist.edu.in>
2026-02-24 16:43:58 +0530

    UseCase9PalindromeCheckerApp

---
+++ src/UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+public class UseCase9PalindromeCheckerApp {
+
+    // Recursive method
+    public static boolean isPalindrome(String input, int start, int end) {
+
+        // Base condition: If pointers cross or meet
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don’t match
+        if (input.charAt(start) != input.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(input, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
